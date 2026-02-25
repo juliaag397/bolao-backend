8de054bd2934eaa0d99e1ddcb602b3d775f66147
Julia <juliaalbuquerquegarcia397@gmail.com>
2026-02-25 16:53:23 -0300

    Alterando conexão para Supabase pooler

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:%40BolaoCopa123@db.yzupebeumeysycnczrwr.supabase.co:5432/postgres"
+  connectionString: "postgresql://postgres.yzupebeumeysycnczrwr:%40Supabase123@aws-1-us-east-1.pooler.supabase.com:6543/postgres"
 });
 
 module.exports = pool;
