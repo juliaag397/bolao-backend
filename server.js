3b434ad8f906cc70f49445ab52314c190c51302c
Julia <juliaalbuquerquegarcia397@gmail.com>
2026-02-27 18:47:12 -0300

    q raiva

--- server.js
+++ server.js
@@ -19,8 +19,12 @@ const app = express();
 
 app.use(cors({
   origin: true,
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
 }));
+
+app.options("*", cors());
 
 app.use(express.json());
 
